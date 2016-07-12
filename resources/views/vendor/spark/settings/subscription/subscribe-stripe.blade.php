21a4daaa40e26a26bd0603ce18b526aa45b8a94d
Beverly Tang <beverlytang@gmail.com>
2016-07-11 22:47:51 -0700

    Hide name field and terms checkbox from registration step 1. Font files. More theming for forms.

--- resources/views/vendor/spark/settings/subscription/subscribe-stripe.blade.php
+++ resources/views/vendor/spark/settings/subscription/subscribe-stripe.blade.php
@@ -5,7 +5,7 @@
     @include('spark::settings.subscription.subscribe-common')
 
     <!-- Billing Information -->
-    <div class="panel panel-default">
+    <div class="panel panel-default panel-form">
         <div class="panel-heading">Billing Information</div>
 
         <div class="panel-body">
@@ -18,6 +18,9 @@
             <form class="form-horizontal" role="form">
                 <!-- Billing Address Fields -->
                 @if (Spark::collectsBillingAddress())
+
+                    <input type="checkbox" name="same_as_delivery"> Same as Delivery
+                    
                     <h2><i class="fa fa-btn fa-map-marker"></i>Billing Address</h2>
 
                     @include('spark::settings.subscription.subscribe-address')
@@ -27,19 +30,17 @@
 
                 <!-- Cardholder's Name -->
                 <div class="form-group">
-                    <label for="name" class="col-md-4 control-label">Cardholder's Name</label>
 
                     <div class="col-md-6">
-                        <input type="text" class="form-control" name="name" v-model="cardForm.name">
+                        <input type="text" class="form-control" name="name" v-model="cardForm.name" placeholder="Cardholder's Name">
                     </div>
                 </div>
 
                 <!-- Card Number -->
                 <div class="form-group" :class="{'has-error': cardForm.errors.has('number')}">
-                    <label for="number" class="col-md-4 control-label">Card Number</label>
 
                     <div class="col-sm-6">
-                        <input type="text" class="form-control" name="number" data-stripe="number" v-model="cardForm.number">
+                        <input type="text" class="form-control" name="number" data-stripe="number" v-model="cardForm.number" placeholder="Card Number">
 
                         <span class="help-block" v-show="cardForm.errors.has('number')">
                             @{{ cardForm.errors.get('number') }}
@@ -49,45 +50,39 @@
 
                 <!-- Security Code -->
                 <div class="form-group">
-                    <label for="number" class="col-md-4 control-label">Security Code</label>
 
                     <div class="col-sm-6">
-                        <input type="text" class="form-control" name="cvc" data-stripe="cvc" v-model="cardForm.cvc">
+                        <input type="text" class="form-control" name="cvc" data-stripe="cvc" v-model="cardForm.cvc" placeholder="Security Code">
                     </div>
                 </div>
 
                 <!-- Expiration -->
                 <div class="form-group">
-                    <label class="col-md-4 control-label">Expiration</label>
 
                     <!-- Month -->
                     <div class="col-md-3">
-                        <input type="text" class="form-control" name="month"
-                            placeholder="MM" maxlength="2" data-stripe="exp-month" v-model="cardForm.month">
+                        <input type="text" class="form-control" name="month" maxlength="2" data-stripe="exp-month" v-model="cardForm.month" placeholder="Expiration MM">
                     </div>
 
                     <!-- Year -->
                     <div class="col-md-3">
-                        <input type="text" class="form-control" name="year"
-                            placeholder="YYYY" maxlength="4" data-stripe="exp-year" v-model="cardForm.year">
+                        <input type="text" class="form-control" name="year" maxlength="4" data-stripe="exp-year" v-model="cardForm.year" placeholder="Expiration YYYY">
                     </div>
                 </div>
 
                 <!-- ZIP Code -->
                 <div class="form-group" v-if=" ! spark.collectsBillingAddress">
-                    <label for="number" class="col-md-4 control-label">ZIP / Postal Code</label>
 
                     <div class="col-sm-6">
-                        <input type="text" class="form-control" name="zip" v-model="cardForm.zip">
+                        <input type="text" class="form-control" name="zip" v-model="cardForm.zip" placeholder="Zip">
                     </div>
                 </div>
 
                 <!-- Coupon -->
                 <div class="form-group" :class="{'has-error': form.errors.has('coupon')}">
-                    <label class="col-md-4 control-label">Coupon</label>
 
                     <div class="col-sm-6">
-                        <input type="text" class="form-control" v-model="form.coupon">
+                        <input type="text" class="form-control" v-model="form.coupon" placeholder="Coupon">
 
                         <span class="help-block" v-show="form.errors.has('coupon')">
                             @{{ form.errors.get('coupon') }}
@@ -97,7 +92,6 @@
 
                 <!-- Tax / Price Information -->
                 <div class="form-group" v-if="spark.collectsEuropeanVat && countryCollectsVat && selectedPlan">
-                    <label class="col-md-4 control-label">&nbsp;</label>
 
                     <div class="col-md-6">
                         <div class="alert alert-info" style="margin: 0;">
