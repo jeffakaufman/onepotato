a59b9ee3f2ba68de1484a091f768ff96403218e1
Matt Kirkpatrick <mattkirkpatrick@gmail.com>
2016-07-13 13:58:40 -0400

    new

--- database/migrations/2016_07_13_164858_subinvoices.php
+++ database/migrations/2016_07_13_164858_subinvoices.php
@@ -17,16 +17,16 @@ class Subinvoices extends Migration
 				
 	            $table->increments('id');
 	            $table->timestamps();
-				$table->string('stripe_event_id',255);
-				$table->string('stripe_customer_id',255);
-				$table->string('stripe_sub_id',255);
-				$table->string('stripe_invoice_type',255);
-				$table->datetime('charge_date');
-				$table->string('charge_amount',255);
-				$table->string('plan_id',255);
-				$table->integer('user_id');
-				$table->string('raw_json',3000);
-				$table->string('invoice_status',255);
+				$table->string('stripe_event_id',255)->nullable();
+				$table->string('stripe_customer_id',255)->nullable();
+				$table->string('stripe_sub_id',255)->nullable();
+				$table->string('stripe_invoice_type',255)->nullable();
+				$table->datetime('charge_date')->nullable();
+				$table->string('charge_amount',255)->nullable();
+				$table->string('plan_id',255)->nullable();
+				$table->integer('user_id')->nullable();
+				$table->string('raw_json',3000)->nullable();
+				$table->string('invoice_status',255)->nullable();
 				
 	        });
 		
