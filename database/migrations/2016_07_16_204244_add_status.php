3cc6e9a6fdebcc3e1b4e2ee98cc4b7a72ad4a5fe
vagrant <vagrant@homestead>
2016-07-16 20:45:03 +0000

    add status to users table

---
+++ database/migrations/2016_07_16_204244_add_status.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+			$table->string('status',255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
