a027214fe24e219d6c6dfe88c44dd11272d776fe
Beverly Tang <beverlytang@gmail.com>
2016-07-31 14:26:28 -0700

    remove hash after page load

--- resources/assets/js/components/account.js
+++ resources/assets/js/components/account.js
@@ -43,10 +43,22 @@ Vue.component('account', {
 var url = document.location.toString();
 if (url.match('#')) {
     $('.nav-sidebar a[href="#' + url.split('#')[1] + '"]').tab('show');
-    console.log('yes');
+    removeHash();
 } 
 
-// Change hash for page-reload
-$('.nav-sidebar a').on('shown.bs.tab', function (e) {
-    window.location.hash = e.target.hash;
-});
+function removeHash() {
+    var scrollV, scrollH, loc = window.location;
+    if ('replaceState' in history) {
+        history.replaceState('', document.title, loc.pathname + loc.search);
+    } else {
+        // Prevent scrolling by storing the page's current scroll offset
+        scrollV = document.body.scrollTop;
+        scrollH = document.body.scrollLeft;
+
+        loc.hash = '';
+
+        // Restore the scroll offset, should be flicker free
+        document.body.scrollTop = scrollV;
+        document.body.scrollLeft = scrollH;
+    }
+}
