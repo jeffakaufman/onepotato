3f226e79ff613e0cf7b5a812714008da52b66269
Beverly Tang <beverlytang@gmail.com>
2016-08-09 22:51:30 -0700

    congrats

--- resources/views/register/congrats.blade.php
+++ resources/views/register/congrats.blade.php
@@ -51,59 +51,58 @@
     </div>
 
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+        
+        <div class="panel panel-default">
 
-                <div class="panel-heading text-center extrapadding">Meals to be delivered
-                    <div class="panel-subtitle"></div>
-                </div>
-                <div class="panel-body font16 extrapadding text-center">
-                    
-                    <div id="congrats" class="row" data-start-date="{{ $start_date }}" data-meal1="{{ $meal1 }}" data-meal2="{{ $meal2 }}" data-meal3="{{ $meal3 }}">
+            <div class="panel-heading text-center extrapadding">Meals to be delivered
+                <div class="panel-subtitle"></div>
+            </div>
+            <div class="panel-body font16 extrapadding text-center">
+                
+                <div id="congrats" class="row" data-start-date="{{ $start_date }}" data-meal1="{{ $meal1 }}" data-meal2="{{ $meal2 }}" data-meal3="{{ $meal3 }}">
+                        
+                    <div id="menu" class="col-xs-12 extrapadding">
+
+                        <div class="meal col-xs-4 font11 thinpadding" v-for="meal in firstMenu" track-by="id">
+                            <a href="#" data-toggle="modal" data-target="#imagemodal-@{{ meal.id }}"><img :src="meal.image" alt="" class="meal_image"></a>
+                            <div class="col-xs-9 col-xs-offset-1 padding nosidepadding text-center">@{{ meal.menu_title }}</div>
                             
-                        <div id="menu" class="col-xs-12 extrapadding">
-
-                            <div class="meal col-xs-4 font11 thinpadding" v-for="meal in firstMenu" track-by="id">
-                                <a href="#" data-toggle="modal" data-target="#imagemodal-@{{ meal.id }}"><img :src="meal.image" alt="" class="meal_image"></a>
-                                <div class="col-xs-9 col-xs-offset-1 padding nosidepadding text-center">@{{ meal.menu_title }}</div>
-                                
-                                <div id="imagemodal-@{{ meal.id }}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-                                  <div class="modal-dialog">
-                                    <div class="modal-content">
-                                      <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                                        <h4 class="modal-title">@{{ meal.menu_title }}</h4>
-                                      </div>
-                                      <div class="modal-body">
-                                        <img :src="meal.image" id="imagepreview">
-                                      </div>
-                                    </div>
+                            <div id="imagemodal-@{{ meal.id }}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+                              <div class="modal-dialog">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                                    <h4 class="modal-title">@{{ meal.menu_title }}</h4>
+                                  </div>
+                                  <div class="modal-body">
+                                    <img :src="meal.image" id="imagepreview">
                                   </div>
                                 </div>
+                              </div>
                             </div>
-                            
                         </div>
-
+                        
                     </div>
-                            
-                </div>
 
+                </div>
+                        
             </div>
-        </div>
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default panel-form">
 
-                <div class="panel-heading text-left extrapadding">Log in
-                    <div class="panel-subtitle">Please use your email address to log into our site to manage
-your account. You will also need to create a password.</div>
-                </div>
-                <div class="panel-body font16 extrapadding text-center">
-                
-                    <button type="submit" class="btn btn-primary" onclick="location.href='/login';">Log in</button>
-                </div>
-
-            </div>
         </div>
     </div>
+    <!-- <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default panel-form">
+
+            <div class="panel-heading text-left extrapadding">Log in
+                <div class="panel-subtitle">Please use your email address to log into our site to manage
+your account.</div>
+            </div>
+            <div class="panel-body font16 extrapadding text-center">
+            
+                <button type="submit" class="btn btn-primary" onclick="location.href='/login';">Log in</button>
+            </div>
+
+        </div>
+    </div> -->
 </div>
 @endsection
