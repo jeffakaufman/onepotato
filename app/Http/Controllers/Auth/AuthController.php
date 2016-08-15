54791c5c80958013cde78fdf57b1e77804bb8d6a
Beverly Tang <beverlytang@gmail.com>
2016-08-15 15:09:04 -0700

    redirect user to delivery-schedule after login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/delivery-schedule';
 
     /**
      * Create a new authentication controller instance.
