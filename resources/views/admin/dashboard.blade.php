7ce65c8468b85b489d3f3e7305df96aca64ede6c
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2016-08-15 11:37:25 -0700

    dashboard reports

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -17,21 +17,21 @@
     <div class="container">
         <!-- Application Dashboard -->
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-4">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-heading"><strong>Upcoming Meals</strong></div>
                     <div class="panel-body">
-                    
                     @foreach ($menus as $menu)
                     	@if ( ($oldDate !=  $menu->delivery_date))
+                    	@if ($menu != reset($menus)) <br /> @endif
                     	<div class="row">
                     		<div class="col-sm-5"> 
-                    		<strong>{{ $menu->delivery_date }}</strong>
+                    			<strong>{{ $menu->delivery_date }}</strong>
                     		</div>
                     	</div>
                     	@endif
                     	<div class="row">
-                    		<div class="col-sm-5">
+                    		<div class="col-sm-10">
                     			{{ $menu->menu_title }}
 		            		@if($menu->hasBeef)
 		            			(Beef)
@@ -52,7 +52,7 @@
 	            				(Shellfish)
 							@endif
                     		</div>
-                    		<div class="col-sm-2">
+                    		<div class="col-sm-2 text-right">
                     			{{ $menu->total }}
                     		</div>
                     	</div>                  	
@@ -61,50 +61,96 @@
                     		$oldDate = $menu->delivery_date;
                     	?>
                     @endforeach
-                    	<div class="row">
-                    		<div class="col-sm-5"> 
-                    			<strong>Omnivore Meals</strong>
-                    		</div>
-                    	</div>
-                    	<div class="row">
-                    		<div class="col-sm-5"> 
-                    			Beef: {{$meat->beef}}<br />
-                    			Poultry: {{$meat->poultry}}<br />
-                    			Fish: {{$meat->fish}}<br />
-                    			Lamb: {{$meat->lamb}}<br />
-                    			Pork: {{$meat->pork}}<br />
-                    			Shellfish: {{$meat->shellfish}}<br />
-                    		</div>
-                    	</div>
-                    
-                    	<div class="row">
-                    		<div class="col-sm-5"> 
-                    			<strong>Upcoming New User Start Dates</strong>
-                    		</div>	     
-                    	</div>	        
-                    @foreach ($newSubs as $newSub)
-	                   	<div class="row">
-                    		<div class="col-sm-2">
-                    		{{ date('F dS', strtotime($newSub->start_date)) }}
-                    	    </div>
-                    		<div class="col-sm-1">
-                    		{{$newSub->total}}
-                    	    </div>
-                    	</div>	
-                    @endforeach
-
                     </div>
-					
-
-
                 </div>
             </div>
-        </div>
+               <div class="row">  
+            <div class="col-sm-2"> 
+                <div class="panel panel-default">
+                    <div class="panel-heading"><strong>Omnivore Meals</strong></div>
+                    <div class="panel-body">
+                    	<div class="row">
+                    		<div class="col-sm-5"> 
+                    			Beef
+                    		</div>	
+                    		<div class="col-sm-5 text-right"> 
+                    			{{$meat->beef}}
+                    		</div>	
+                    	</div>
+                    	<div class="row">	
+                    		<div class="col-sm-5">
+                    			Poultry
+                    		</div>
+                    		<div class="col-sm-5 text-right"> 
+                    			{{$meat->poultry}}
+                    		</div>	
+                    	</div>
+                    	<div class="row">
+                    		<div class="col-sm-5">
+                    			Fish
+                    		</div>
+                    		<div class="col-sm-5 text-right"> 
+                    			{{$meat->fish}}
+                    		</div>	
+                    	</div>
+                    	<div class="row">
+                    		<div class="col-sm-5">
+                    			Lamb
+                    		</div>
+                    		<div class="col-sm-5 text-right"> 
+                    			{{$meat->lamb}}
+                    		</div>
+                    	</div>
+                    	<div class="row">	
+                    		<div class="col-sm-5">
+                    			Pork
+                    		</div>
+                    		<div class="col-sm-5 text-right"> 
+                    			{{$meat->pork}}
+                    		</div>	
+                    	</div>
+                    	<div class="row">
+                    		<div class="col-sm-5">
+                    			Shellfish
+                    		</div>
+                    		<div class="col-sm-5 text-right"> 
+                    			{{$meat->shellfish}}
+                    		</div>
+                    	</div>
+                   	</div>
+           		</div>
+           	</div>
+           	</div>
+           	
+           	
+           	<div class="row">
+      		<div class="col-sm-2"> 
+        		<div class="panel panel-default">
+            		<div class="panel-heading"><strong>New Users</strong></div>
+                	<div class="panel-body">
+        			@foreach ($newSubs as $newSub)
+	    				<div class="row">
+            				<div class="col-sm-9">
+                				{{ date('F dS', strtotime($newSub->start_date)) }}
+                			</div>
+               				<div class="col-sm-1">
+                				{{$newSub->total}}
+                			</div>
+            			</div>	
+    				@endforeach
+					</div>
+      			</div>  
+			</div>  
+           	</div>
+			
+			
+			
+			
+			
+			  
+		</div>    
+	</div>               		
 
-
-
-
-    </div>
 </home>
 @endsection
 
