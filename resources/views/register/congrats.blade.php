391701eb962392649b14c98ff91ed52efa1d74f0
Beverly Tang <beverlytang@gmail.com>
2016-08-26 17:00:13 -0700

    date

--- resources/views/register/congrats.blade.php
+++ resources/views/register/congrats.blade.php
@@ -43,7 +43,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading with-subtitle">
                     <h1>Congratulations!
-                        <div class="panel-subtitle">Your first box will arrive on {{ date('F d', strtotime($start_date)) }}.<br>
+                        <div class="panel-subtitle">Your first box will arrive on {{ date('F j', strtotime($start_date)) }}.<br>
                         Get ready to start cooking!</div>
                     </h1>
                 </div>
