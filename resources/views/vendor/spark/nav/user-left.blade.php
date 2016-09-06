529e06e26adba7d5c888c04e5c96721d845498fd
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2016-09-06 12:46:36 -0700

    remove marketplace

--- resources/views/vendor/spark/nav/user-left.blade.php
+++ resources/views/vendor/spark/nav/user-left.blade.php
@@ -3,5 +3,5 @@
     <li @if (Request::is('delivery-schedule'))class="active"@endif><a href="/delivery-schedule">Delivery Schedule</a></li>
     <li @if (Request::is('faq'))class="active"@endif><a href="/faq">FAQ</a></li>
     <li @if (Request::is('whats-cooking'))class="active"@endif><a href="/whats-cooking">What's Cooking?</a></li>
-    <li @if (Request::is('marketplace'))class="active"@endif><a href="/marketplace">Marketplace</a></li>
+   <!-- <li @if (Request::is('marketplace'))class="active"@endif><a href="/marketplace">Marketplace</a></li>-->
 </ul>
