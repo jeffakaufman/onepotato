1eed5945f6f16260ea038d0a8349241a2f2432c7
Matt Kirkpatrick <mattkirkpatrick@gmail.com>
2016-09-28 21:12:11 -0400

    credit webhook

--- app/Http/Controllers/SubInvoiceController.php
+++ app/Http/Controllers/SubInvoiceController.php
@@ -1296,13 +1296,26 @@ class SubinvoiceController extends Controller
 							$credit->save();
 							http_response_code(200);
 					
-					} catch (Exception $e) {
-						  // Something else happened, completely unrelated to Stripe
+					} catch (Stripe_InvalidRequestError $e) {
+						  // Invalid parameters were supplied to Stripe's API
+					} catch (Stripe_AuthenticationError $e) {
+						  // Authentication with Stripe's API failed
+						  // (maybe you changed API keys recently)
+					} catch (Stripe_ApiConnectionError $e) {
+						  // Network communication with Stripe failed
+					} catch (Stripe_Error $e) {
+						  // Display a very generic error to the user, and maybe send
+						  // yourself an email
+						// Something else happened, completely unrelated to Stripe
 							$credit->date_applied = date("Y-m-d H:i:s");  
 							$credit->credit_status = 'stripe_error_not_applied';
 							$credit->stripe_xml = json_encode($event_json);
 							$credit->save();
 							http_response_code(500);
+					} catch (Exception $e) {
+						  // Something else happened, completely unrelated to Stripe
+					} catch (Exception $e) {
+						  
 					}
 			
 				}//end if there is a credit
