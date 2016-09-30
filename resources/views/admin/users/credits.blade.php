a295a4a190045b31e1a0045e4ce00aea90249650
Matt Kirkpatrick <mattkirkpatrick@gmail.com>
2016-09-30 17:49:29 -0400

    credit

--- resources/views/admin/users/credits.blade.php
+++ resources/views/admin/users/credits.blade.php
@@ -33,7 +33,7 @@
 			<table>	
 				<tr><td width="150"><strong>Date Applied</strong></td><td width="100"><strong>Amount</strong></td><td width="100"><strong>Percent</strong></td><td width="200"><strong>Description</strong></td></tr>
 				@foreach ($credits as $credit)
-				<tr><td>{{ $credit->date_applied}}</td><td>{{ number_format($credit->credit_amount / 100,2) }}</td><td>{{ $credit->credit_percent }}</td><td>{{ $credit->credit_description}}
+				<tr><td>{{ $credit->created_at}}</td><td>{{ number_format($credit->credit_amount / 100,2) }}</td><td>{{ $credit->credit_percent }}</td><td>{{ $credit->credit_description}}
 				@endforeach
 			</table>
 			
