c472c29b5d4a7c84604ceb21e84e2949c9a5b66e
Matt Kirkpatrick <mattkirkpatrick@gmail.com>
2016-10-07 13:09:21 -0400

    cancel bug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1341,14 +1341,13 @@ class UserController extends Controller
 		
 		try {
 			$subscription = \Stripe\Subscription::retrieve($stripe_sub_id);
-		}
+		} catch (\Exception $e) {
+            //Do Nothing
+        }
 		
 		if (isset($subscription)) {
 			$subscription->cancel();
 		}
-	
-
-
 
 		$cancel = new Cancellation();
 		
