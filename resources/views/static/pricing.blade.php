3a92d3fd84a47f607cda27d10b26144e51df1e32
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2016-10-22 13:59:17 -0700

    pricing page

---
+++ resources/views/static/pricing.blade.php
@@ -0,0 +1,30 @@
+@extends('spark::layouts.app')
+
+@section('content')
+<div id="static" class="container">
+	<div class="row">
+		<div class="col-xs-12">
+			<h1>Pricing</h1>
+		</div>
+	</div>
+  <div class="row">
+    <div class="col-sm-4 col-sm-offset-1">
+      <p>
+      <h2>Meals per week: <span style="color:black">3</h2>
+      <h2>Price per Adult Meal: <span style="color:black"> $11.99*</h2>
+      <h2>Meals per week: <span style="color:black"> $4.50</span></h2>
+      </p>
+      *Gluten Free pricing of $1.50 per meal per adult
+      <h2 style="text-align: center;"></h2>
+      <div style="text-align: center; height: 65px;"><a class="btn btn-primary" style="font-size:x-large" href="/register">Get Started</a></div>
+      	<div style="text-align: center;">
+ 	   		<div style="text-align: center;"><em>Customize to your family. Shipping is always free.</em></div>
+ 	   		<div style="text-align: center;"><em>Skip any week. Cancel any time.</em></div>
+    	</div>
+    </div>
+	<div class="col-sm-5 col-sm-offset-1">
+    	<img src="/img/pricing1.jpg" alt="" />
+    </div>
+  </div>
+</div>
+@endsection
