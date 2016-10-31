4b737950db7d23ea4a975e1bf1cee432cdc13b0e
Beverly Tang <beverlytang@gmail.com>
2016-10-31 13:06:29 -0700

    please work

--- resources/views/vendor/spark/layouts/app.blade.php
+++ resources/views/vendor/spark/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <!-- Meta Information -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
 
     <title>@yield('title', 'One Potato')</title>
 
